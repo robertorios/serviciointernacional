17427874c32795f9ed33672abeb60d7bce210a33
Roberto <maclaptop@unknown00236c965dca.att.net>
2014-10-25 01:45:12 -0400

    database mysql mysql2 migrations devise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-  <script type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es'}, 'google_translate_element');
-}
-</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
